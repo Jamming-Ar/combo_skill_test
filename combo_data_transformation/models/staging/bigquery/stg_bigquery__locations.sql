d658bb76e2ca45b18d2e82f8b17172f5e09d5f09
jamming.armance <jamming.armance@voltalis.com>
2026-04-16 20:15:46 +0200

    rename stg_bigquery__locations

--- combo_data_transformation/models/staging/bigquery/stg_bigquery__locations.sql
+++ combo_data_transformation/models/staging/bigquery/stg_bigquery__locations.sql
@@ -2,17 +2,17 @@ with
 
 renamed as (
     select
-        id,
-        name,
+        id as location_id,
+        name as location_name,
         account_id,
         created_at,
         updated_at,
-        address,
-        preferences,
-        city,
-        zipcode,
+        address as location_address,
+        preferences as location_preferences,
+        city as location_city,
+        zipcode as location_zipcode,
         collective_agreement_id,
-        country,
+        country as location_country,
         partner_id,
         archived,
         default_break_duration,
@@ -23,20 +23,20 @@ renamed as (
         location_type,
         minimum_shift_duration_for_automatic_breaks_in_hours
     from {{ source('sources', 'locations') }}
-),
+)
 
 select
-    id,
-    name,
+    location_id,
+    location_name,
     account_id,
     created_at,
     updated_at,
-    address,
-    preferences,
-    city,
-    zipcode,
+    location_address,
+    location_preferences,
+    location_city,
+    location_zipcode,
     collective_agreement_id,
-    country,
+    location_country,
     partner_id,
     archived,
     default_break_duration,
