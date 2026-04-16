0af611ff5fff3f9081b922d56e0eef3e10d53cf0
jamming.armance <jamming.armance@voltalis.com>
2026-04-16 09:01:53 +0200

    rename stg_bigquery__memberships

--- combo_data_transformation/models/staging/bigquery/stg_bigquery__memberships.sql
+++ combo_data_transformation/models/staging/bigquery/stg_bigquery__memberships.sql
@@ -2,15 +2,15 @@ with
 
 renamed as (
     select
-        id,
+        id as membership_id,
         user_id,
         account_id,
-        active,
-        role,
+        active as is_active_membership,
+        role as membership_role,
         created_at,
         updated_at,
         sort_index,
-        preferences,
+        preferences as membership_preferences,
         onboarded,
         can_manage_published_planning,
         can_invalidate_shift,
@@ -21,18 +21,18 @@ renamed as (
         enhanced_medical_examination,
         firstname
     from {{ source('sources', 'memberships') }}
-),
+)
 
 select
-    id,
+    membership_id,
     user_id,
     account_id,
-    active,
-    role,
+    is_active_membership,
+    membership_role,
     created_at,
     updated_at,
     sort_index,
-    preferences,
+    membership_preferences,
     onboarded,
     can_manage_published_planning,
     can_invalidate_shift,
